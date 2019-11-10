2df1958dd6f32734c8eec74b90045f60513cc7a0
Xavier Garrido <xavier.garrido@gmail.com>
2019-11-10 11:13:36 +0100

    Fix setup.py
    - provide a setup.py which install fgspectra from github and cobaya from the external_modules
    branch in github
    
    - also add the associated yaml file to get defaults of mflike likelihood
    
    - move the test_mflike.yaml into a tests directory (NB: to run this test via cobaya-run for
      instance, one need to install a theory code either camb or class)

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name="mflike",
-      version='1.0',
-      description='SO LAT multi-frequency likelihood for cobaya',
+      version="1.0",
+      description="SO LAT multi-frequency likelihood for cobaya",
       zip_safe=True,
       packages = find_packages(),
-      install_requires=['numpy', 'fgspectra', 'cobaya', 'sacc']
-      )
-
+      install_requires=[
+          "fgspectra @ git+ssh://git@github.com/simonsobs/fgspectra@master#egg=fgspectra",
+          "cobaya @ git+ssh://git@github.com/CobayaSampler/cobaya@external_modules#egg=cobaya",
+          "sacc"
+      ],
+      package_data={"mflike": ["mflike.yaml"]},
+)
