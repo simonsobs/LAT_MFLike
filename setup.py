0f66814df79a3de14aa9c7cef0ba5298a66f3ac2
Xavier Garrido <xavier.garrido@gmail.com>
2019-11-18 19:10:33 +0100

    switch to devel branch of cobaya where installation process works

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name="mflike",
       packages = find_packages(),
       install_requires=[
           "fgspectra @ git+https://github.com/simonsobs/fgspectra@master#egg=fgspectra",
-          "cobaya @ git+https://github.com/CobayaSampler/cobaya@external_modules#egg=cobaya",
+          "cobaya @ git+https://github.com/CobayaSampler/cobaya@devel#egg=cobaya",
           "sacc"
       ],
       package_data={"mflike": ["MFLike.yaml"]},
