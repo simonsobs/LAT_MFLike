8149c02c3b5f92fd9caaeb8ac79cac72a486ef05
Thibaut <thbtlouis@gmail.com>
2020-01-22 10:44:40 +0100

    rename fg_model to fg_dict

--- mflike/mflike.py
+++ mflike/mflike.py
@@ -336,13 +336,13 @@ def get_foreground_model(fg_params, fg_model, frequencies, ell, requested_cls=["
 
     components = fg_model["components"]
     component_list = {s: components[s] for s in requested_cls}
-    fg_model = {}
+    fg_dict = {}
     for c1, f1 in enumerate(frequencies):
         for c2, f2 in enumerate(frequencies):
             for s in requested_cls:
                 fg_model[s, "all", f1, f2] = np.zeros(len(ell))
                 for comp in component_list[s]:
-                    fg_model[s, comp, f1, f2] = model[s, comp][c1, c2]
-                    fg_model[s, "all", f1, f2] += fg_model[s, comp, f1, f2]
+                    fg_dict[s, comp, f1, f2] = model[s, comp][c1, c2]
+                    fg_dict[s, "all", f1, f2] += fg_model[s, comp, f1, f2]
 
-    return fg_model
+    return fg_dict
