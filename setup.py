96426d5a4dc261192205cf821bf19e19113f2f04
Xavier Garrido <xavier.garrido@gmail.com>
2020-02-27 22:18:40 +0100

    add python version requirement

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
-setup(name="mflike",
-      version="1.0",
-      description="SO LAT multi-frequency likelihood for cobaya",
-      zip_safe=True,
-      packages = find_packages(),
-      install_requires=[
-          "fgspectra @ git+https://github.com/simonsobs/fgspectra@master#egg=fgspectra",
-          "cobaya @ git+https://github.com/CobayaSampler/cobaya@devel#egg=cobaya",
-          "sacc @ git+https://github.com/simonsobs/sacc@master#egg=sacc",
-      ],
-      package_data={"mflike": ["MFLike.yaml"]},
+setup(
+    name="mflike",
+    version="1.0",
+    description="SO LAT multi-frequency likelihood for cobaya",
+    zip_safe=True,
+    packages=find_packages(),
+    python_requires=">=3.5",
+    install_requires=[
+        "fgspectra @ git+https://github.com/simonsobs/fgspectra@master#egg=fgspectra",
+        "cobaya @ git+https://github.com/CobayaSampler/cobaya@devel#egg=cobaya",
+        "sacc @ git+https://github.com/simonsobs/sacc@master#egg=sacc",
+    ],
+    package_data={"mflike": ["MFLike.yaml"]},
 )
