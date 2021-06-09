5f6fa8cfb1c037ed7a636d13c6dedd4c81060de9
umbnat92 <34275212+umbnat92@users.noreply.github.com>
2021-06-09 15:58:45 +0200

    ACT FG model implemented (#27)
    * ACT FG model implemented
    
    ACT 2020 foreground model implemented. It needs fgspectra/act_sz_x_cib branch installed.
    
    * fgspectra act_sx_x_cib repo
    
    Link to the fgspectra/act_sz_x_cib branch
    
    * some typos corrected

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(),
     python_requires=">=3.5",
     install_requires=[
-        "fgspectra @ git+https://github.com/simonsobs/fgspectra@master#egg=fgspectra",
+        "fgspectra @ git+https://github.com/simonsobs/fgspectra@act_sz_x_cib#egg=fgspectra",
         "cobaya>=3.0.4",
         "sacc>=0.4.2",
     ],
