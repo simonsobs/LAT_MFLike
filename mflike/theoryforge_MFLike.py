8f99d8fe1cedc4160b117c762ff34f4023b896cf
Umberto Natale <34275212+umbnat92@users.noreply.github.com>
2021-10-21 15:47:34 +0200

    simple average for external bandpasses (#37)
    * Generality restored
    
    Incompatibility with the generality of MFLike structure resolved
    
    * simple average for external bandpasses

--- mflike/theoryforge_MFLike.py
+++ mflike/theoryforge_MFLike.py
@@ -21,7 +21,22 @@ def _get_fr(array):
         fr = 150
     if a == "PA3":
         fr = array.split("_")[3]
-    return fr
+    if a == 'PA4' or a == 'PA5' or a == 'PA6':
+        fr = array.split("_")[3].replace("f", "")
+    return np.int(fr), a
+
+def _get_arrays_weights(arrays,polarized_arrays,freqs):
+    array_weights = {}
+    counter = []
+    for array in arrays:
+        fr,pa = _get_fr(array)
+        if (pa in polarized_arrays) and (fr in freqs):
+            if fr not in counter:
+                counter.append(fr)
+                array_weights[fr] = 1
+            else:
+                array_weights[fr] = array_weights[fr]+1
+    return array_weights 
 
 
 class TheoryForge_MFLike:
@@ -80,12 +95,15 @@ class TheoryForge_MFLike:
             self.bandint_external_bandpass = mflike.band_integration[
                 "external_bandpass"
             ]
+            self.polarized_arrays = mflike.band_integration["polarized_arrays"]
 
             # Bandpass construction for band integration
             if self.bandint_external_bandpass:
-                path = os.path.normpath(os.path.join(self.data_folder, "/bp_int/"))
+                path = os.path.normpath(os.path.join(self.data_folder,
+                                                       'external_bandpasses/'))
                 arrays = os.listdir(path)
-                self._init_external_bandpass_construction(arrays)
+                self._init_external_bandpass_construction(path,arrays)
+                self.array_weights = _get_arrays_weights(arrays,self.polarized_arrays,self.freqs)
 
     def get_modified_theory(self, Dls, **params):
 
@@ -97,11 +115,22 @@ class TheoryForge_MFLike:
         # "bandint_shift_{ch}" is not explored in the MCMC
         # this might be optimized in the future
         if self.bandint_external_bandpass:
-            self.bandint_freqs = self._external_bandpass_construction(**nuis_params)
+            order,self.bandint_freqs = self._external_bandpass_construction(**nuis_params)
+            fg_dict = self._get_foreground_model(freqs_order=order,**fg_params)
+
+            for f1 in self.freqs:
+                for f2 in self.freqs:
+                    for s in self.requested_cls:
+                        fg_dict[s,'all',f1,f2] = np.zeros(len(self.l_bpws))
+                        for pa in self.polarized_arrays:
+                            o1 = pa+'_'+str(f1)
+                            o2 = pa+'_'+str(f2)
+                            if (o1 in order) and (o2 in order):
+                                fg_dict[s,'all',f1,f2] += fg_dict[s,'all',o1,o2]/self.array_weights[f1]/self.array_weights[f2]
+
         else:
             self.bandint_freqs = self._bandpass_construction(**nuis_params)
-
-        fg_dict = self._get_foreground_model(**fg_params)
+            fg_dict = self._get_foreground_model(**fg_params)
 
         cmbfg_dict = {}
         # Sum CMB and FGs
@@ -176,7 +205,7 @@ class TheoryForge_MFLike:
         self.fg_component_list = {s: components[s] for s in self.requested_cls}
 
     # Gets the actual power spectrum of foregrounds given the passed parameters
-    def _get_foreground_model(self, ell=None, **fg_params):
+    def _get_foreground_model(self,ell = None,freqs_order=None,**fg_params):
         # if ell = None, it uses the l_bpws, otherwise the ell array provided
         # useful to make tests at different l_max than the data
         if not hasattr(ell, "__len__"):
@@ -266,8 +295,12 @@ class TheoryForge_MFLike:
         )
 
         fg_dict = {}
-        for c1, f1 in enumerate(self.freqs):
-            for c2, f2 in enumerate(self.freqs):
+        if not hasattr(freqs_order, "__len__"):
+            freqs = self.freqs
+        else:
+            freqs = freqs_order
+        for c1, f1 in enumerate(freqs):
+            for c2, f2 in enumerate(freqs):
                 for s in self.requested_cls:
                     fg_dict[s, "all", f1, f2] = np.zeros(len(ell))
                     for comp in self.fg_component_list[s]:
@@ -283,6 +316,7 @@ class TheoryForge_MFLike:
                         else:
                             fg_dict[s, comp, f1, f2] = model[s, comp][c1, c2]
                             fg_dict[s, "all", f1, f2] += fg_dict[s, comp, f1, f2]
+                    
         return fg_dict
 
     # Takes care of the bandpass construction. It returns a list of nu-transmittance for each frequency or an array with the effective freqs.
@@ -325,23 +359,27 @@ class TheoryForge_MFLike:
 
         return bandint_freqs
 
-    def _init_external_bandpass_construction(self, arrays):
+    def _init_external_bandpass_construction(self,path,arrays):
         self.external_bandpass = []
         for array in arrays:
-            fr = _get_fr(array)
-            nu_ghz, bp = np.loadtxt(array, usecols=(0, 1), unpack=True)
-            trans_norm = np.trapz(bp * _cmb2bb(nu_ghz), nu_ghz)
-            self.external_bandpass.append([fr, nu_ghz, bp / trans_norm])
+            fr,pa = _get_fr(array)
+            if pa in self.polarized_arrays and fr in self.freqs:
+                nu_ghz, bp = np.loadtxt(path+'/'+array,usecols=(0,1),unpack=True)
+                trans_norm = np.trapz(bp * _cmb2bb(nu_ghz), nu_ghz)
+                self.external_bandpass.append([pa,fr,nu_ghz,bp/trans_norm])
+            
 
     def _external_bandpass_construction(self, **params):
         bandint_freqs = []
-        for fr, nu_ghz, bp in self.external_bandpass:
+        order = []
+        for pa,fr,nu_ghz,bp in self.external_bandpass:
             bandpar = "bandint_shift_" + str(fr)
             nub = nu_ghz + params[bandpar]
             trans = bp * _cmb2bb(nub)
             bandint_freqs.append([nub, trans])
+            order.append(pa+'_'+str(fr))
 
-        return bandint_freqs
+        return order,bandint_freqs
 
     ###########################################################################
     ## This part deals with calibration factors
