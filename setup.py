5cc978ae45bdcba17d7d04f3db1260b4c31a22a1
Xavier Garrido <xavier.garrido@gmail.com>
2021-10-22 15:29:40 +0200

    add versioneer to support version tag (#38)

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import find_packages, setup
 
+import versioneer
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
 setup(
     name="mflike",
-    version="1.0",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description="SO LAT multi-frequency likelihood for cobaya",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
     zip_safe=True,
     packages=find_packages(),
     python_requires=">=3.5",
